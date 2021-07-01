d5d5ea08a13e5f70180e398124fef73d6782061c
FredTams79 <fred.tamarelle@gmail.com>
2021-07-02 00:45:34 +0200

    update app.js

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const path = require("path");
 
-const stuffRoutes = require("./routes/stuff");
+const sauceRoutes = require("./routes/sauce");
 const userRoutes = require("./routes/user");
 
 mongoose
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use("/api/stuff", stuffRoutes);
+app.use("/api/sauce", sauceRoutes);
 app.use("/api/auth", userRoutes);
 
 module.exports = app; // exporter cette application
